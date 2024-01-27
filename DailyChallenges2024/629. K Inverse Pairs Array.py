e0cee084a387cbb17004c84994cf93356798dceb
e-mm-u <uh.habiba1012@gmail.com>
2024-01-27 23:39:32 +0600

    27-jan-24

---
+++ DailyChallenges2024/629. K Inverse Pairs Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        MOD = 10**9 + 7
+        dp = [[0]*(k+1) for _ in range(n+1)]
+        dp[1][0] = 1
+        for i in range(2, n+1):
+            dp[i][0] = 1
+            for j in range(1, k+1):
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % MOD
+                if j >= i:
+                    dp[i][j] = (dp[i][j] - dp[i-1][j-i] + MOD) % MOD
+        return dp[n][k]
